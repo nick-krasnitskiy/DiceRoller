74db84c8f3a7ef7286c6b9cbcb7661a906ec25a6
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2024-06-26 22:24:56 +0300

    Initial Commit

---
+++ DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by Nick Krasnitskiy on 26.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
