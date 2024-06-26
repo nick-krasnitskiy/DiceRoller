01d7323695ed5f8ab55b20c1a784f48bd6ecd779
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2024-06-26 22:38:19 +0300

    Use state to update a view.

--- DiceRoller/DiceView.swift
+++ DiceRoller/DiceView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct DiceView: View {
-    var numberOfPips: Int = 1
+    @State private var numberOfPips: Int = 1
     
     var body: some View {
         VStack {
@@ -17,8 +17,11 @@ struct DiceView: View {
             .frame(width: 100, height: 100)
             
             Button("Roll") {
-                numberOfPips = Int.random(in: 1...6)
+                withAnimation {
+                    numberOfPips = Int.random(in: 1...6)
+                }
             }
+            .buttonStyle(.bordered)
         }
     }
 }
