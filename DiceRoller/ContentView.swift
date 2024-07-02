51475f1977a217c46f55ed98bcf4c5bc90a77d08
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2024-07-02 10:58:19 +0300

    Use images in button labels.

--- DiceRoller/ContentView.swift
+++ DiceRoller/ContentView.swift
@@ -22,17 +22,19 @@ struct ContentView: View {
             }
             
             HStack {
-                Button("Remove Dice") {
+                Button("Remove Dice", systemImage: "minus.circle.fill") {
                     numberOfDice -= 1
                 }
                 .disabled(numberOfDice == 1)
                 
-                Button("Add Dice") {
+                Button("Add Dice", systemImage: "plus.circle.fill") {
                     numberOfDice += 1
                 }
                 .disabled(numberOfDice == 5)
             }
             .padding()
+            .labelStyle(.iconOnly)
+            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
         }
         .padding()
     }
